5687da0ad4a10cbadfcd9e33518c593638901b24
kartsridhar <contactkarthik34@gmail.com>
2019-10-07 18:53:54 +0100

    time reduced to 3.97s after combining all += operations

--- CW1/stencil.c
+++ CW1/stencil.c
@@ -61,11 +61,13 @@ void stencil(const int nx, const int ny, const int width, const int height,
 {
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < nx + 1; ++i) {
-      tmp_image[j + i * height] =  image[j     + i       * height] * 0.6;
-      tmp_image[j + i * height] += image[j     + (i - 1) * height] * 0.1;
-      tmp_image[j + i * height] += image[j     + (i + 1) * height] * 0.1;
-      tmp_image[j + i * height] += image[j - 1 + i       * height] * 0.1;
-      tmp_image[j + i * height] += image[j + 1 + i       * height] * 0.1;
+      tmp_image[j + i * height] =  (image[j     + i       * height] * 0.6) + (image[j     + (i - 1) * height] * 0.1)
+                                +  (image[j     + (i + 1) * height] * 0.1) + (image[j - 1 + i       * height] * 0.1)
+                                +  (image[j + 1 + i       * height] * 0.1);
+      // tmp_image[j + i * height] += image[j     + (i - 1) * height] * 0.1;
+      // tmp_image[j + i * height] += image[j     + (i + 1) * height] * 0.1;
+      // tmp_image[j + i * height] += image[j - 1 + i       * height] * 0.1;
+      // tmp_image[j + i * height] += image[j + 1 + i       * height] * 0.1;
     }
   }
 }
