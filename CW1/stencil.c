d535a7517f3c84931dc0fec89341e92638fccbd7
Karthik Sridhar <ks17226@bc4login1.bc4.acrc.priv>
2019-10-12 12:08:31 +0100

    no major change

--- CW1/stencil.c
+++ CW1/stencil.c
@@ -61,11 +61,8 @@ void stencil(const int nx, const int ny, const int width, const int height,
 {
   for (int i = 1; i < nx + 1; ++i) {
     for (int j = 1; j < ny + 1; ++j) {
-      // tmp_image[j + i * height] =  (image[j     + i       * height] * 0.6) + (image[j     + (i - 1) * height] * 0.1)
-      //                           +  (image[j     + (i + 1) * height] * 0.1) + (image[j - 1 + i       * height] * 0.1)
-      //                           +  (image[j + 1 + i       * height] * 0.1);
-      int cell = j + i * height;
-      tmp_image[cell] = image[cell] * 0.6 + (image[cell - height] + image[cell + height] + image[cell - 1] +  image[cell + 1]) * 0.1;
+       int cell = j + i * height;
+       tmp_image[cell] = image[cell] * 0.6 + (image[cell - height] + image[cell + height] + image[cell - 1] +  image[cell + 1]) * 0.1;      
     }
   }
 }
