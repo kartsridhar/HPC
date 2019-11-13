dea48a73209abd7bf82ba377a0a27cfe193f26b9
kartsridhar <contactkarthik34@gmail.com>
2019-11-13 10:53:09 +0000

    removed worker global variable

--- CW2/stencil.c
+++ CW2/stencil.c
@@ -68,8 +68,6 @@ int main(int argc, char* argv[])
   MPI_Comm_size( MPI_COMM_WORLD, &size );
   MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 
-  workers = size;
-
   int ii,jj;             /* row and column indices for the section */
 
   /* 
